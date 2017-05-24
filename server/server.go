b95f38d492c078ec6d496c5dfba5e707a71d3f72
gavbn <wjw69763@xzsok.com>
2017-05-24 17:22:20 +0530

    Update server.go

--- server/server.go
+++ server/server.go
@@ -14,8 +14,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/jpillora/cloud-torrent/engine"
-	"github.com/jpillora/cloud-torrent/static"
+	"github.com/gavbn/bbth/engine"
+	"github.com/gavbn/bbth/static"
 	"github.com/jpillora/cookieauth"
 	"github.com/jpillora/gziphandler"
 	"github.com/jpillora/requestlog"
